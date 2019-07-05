ad87900498a4c3de7a82924916daad31535ba582
Paul Hosler <pahosler@gmail.com>
2019-07-05 17:58:26 -0500

    add BenefitCards

---
+++ src/components/BenefitCards/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const BenefitCards = ({ data }) => (
+  <div className="benefit-cards">
+    {data.map((card,i) => (
+      <div key={card.heading} className={`card-container ${i === 0 ? 'card-active' : ''}`}>
+        <div className="card-content">
+          <div className="card-title">
+            <h3>
+              {i + 1}. {card.heading}
+            </h3>
+          </div>
+          <div className="card-text">
+            <p>{card.content}</p>
+          </div>
+        </div>
+      </div>
+    ))}
+  </div>
+);
+
+BenefitCards.propTypes = {
+  data: PropTypes.arrayOf(
+    PropTypes.shape({
+      heading: PropTypes.string,
+      content: PropTypes.array
+    })
+  )
+};
+
+export default BenefitCards;
