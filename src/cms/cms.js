512b29ebd6456d8b49c7cdb86acf41bd367820ea
Paul Hosler <pahosler@gmail.com>
2019-07-04 23:43:03 -0500

    refactoring

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -6,7 +6,7 @@ import ArticlePreview from './preview-templates/ArticlePreview'
 import PricingPagePreview from './preview-templates/PricingPagePreview'
 import ContactPagePreview from './preview-templates/ContactPagePreview'
 
-CMS.registerPreviewStyle('/styles.css')
+CMS.registerPreviewStyle('/cms.css')
 CMS.registerPreviewTemplate('home', HomePagePreview)
 CMS.registerPreviewTemplate('about', AboutPagePreview)
 CMS.registerPreviewTemplate('pricing', PricingPagePreview)
