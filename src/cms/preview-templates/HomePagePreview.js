531ce507253bda757ce8eb3988c6e79871dd65be
Paul Hosler <pahosler@gmail.com>
2019-07-07 09:15:03 -0500

    edit test

--- src/cms/preview-templates/HomePagePreview.js
+++ src/cms/preview-templates/HomePagePreview.js
@@ -18,7 +18,6 @@ const HomePagePreview = ({ entry, getAsset }) => {
 
   return (
     <HomePageTemplate
-      // title={entry.getIn(['data', 'title'])}
       meta_title={entry.getIn(['data', 'meta_title'])}
       meta_description={entry.getIn(['data', 'meta_description'])}
       hero={{
