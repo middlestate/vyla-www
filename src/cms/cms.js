37cd6f5d4ef3caa829fa39520e06eba3f6702eb5
pahosler <pahosler@gmail.com>
2020-08-06 18:19:39 -0500

    is it working again???

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,12 +1,12 @@
-import CMS from 'netlify-cms';
-import '../assets/sass/styles.scss';
+import CMS from 'netlify-cms-app';
+import styles from '!css-loader!sass-loader!../assets/sass/styles.scss';
 import HomePagePreview from './preview-templates/HomePagePreview';
 import AboutPagePreview from './preview-templates/AboutPagePreview';
 import FarmPagePreview from './preview-templates/FarmPagePreview';
 import ArticlePreview from './preview-templates/ArticlePreview';
 import ContactPagePreview from './preview-templates/ContactPagePreview';
 
-// CMS.registerPreviewStyle(styles.toString(), { raw: false });
+CMS.registerPreviewStyle(styles.toString(), { raw: false });
 CMS.registerPreviewTemplate('home', HomePagePreview);
 CMS.registerPreviewTemplate('about', AboutPagePreview);
 CMS.registerPreviewTemplate('thefarm', FarmPagePreview);
