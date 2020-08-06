f3ea425d566ed84165d2974166047f2332d99267
pahosler <pahosler@gmail.com>
2020-08-06 16:20:50 -0500

    rebuild pacckage.json

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,4 +1,4 @@
-import CMS from 'netlify-cms-app';
+import CMS from 'netlify-cms';
 import '../assets/sass/styles.scss';
 import HomePagePreview from './preview-templates/HomePagePreview';
 import AboutPagePreview from './preview-templates/AboutPagePreview';
