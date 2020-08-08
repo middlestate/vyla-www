2f85f9a85518f034c93534fa3ef631b68f555774
pahosler <pahosler@gmail.com>
2020-08-08 16:10:44 -0500

    init dev

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -6,7 +6,7 @@ import FarmPagePreview from './preview-templates/FarmPagePreview';
 import ArticlePreview from './preview-templates/ArticlePreview';
 import ContactPagePreview from './preview-templates/ContactPagePreview';
 
-CMS.registerPreviewStyle(styles.toString(), { raw: false });
+CMS.registerPreviewStyle(styles.toString(), { raw: true });
 CMS.registerPreviewTemplate('home', HomePagePreview);
 CMS.registerPreviewTemplate('about', AboutPagePreview);
 CMS.registerPreviewTemplate('thefarm', FarmPagePreview);
