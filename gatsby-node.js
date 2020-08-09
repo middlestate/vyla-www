002ecb47fb0b41ef2bf7392b8646433b622ab6ab
pahosler <pahosler@gmail.com>
2020-08-09 02:09:44 -0500

    edit gatsby-node

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,14 +1,17 @@
-const _ = require('lodash')
-const path = require('path')
-const {createFilePath} = require('gatsby-source-filesystem')
-const createPaginatedPages = require('gatsby-paginate')
+const _ = require('lodash');
+const path = require('path');
+const { createFilePath } = require('gatsby-source-filesystem');
+const createPaginatedPages = require('gatsby-paginate');
 
-exports.createPages = ({actions, graphql}) => {
-  const {createPage} = actions
+exports.createPages = ({ actions, graphql }) => {
+  const { createPage } = actions;
 
   return graphql(`
     {
-      allMarkdownRemark(limit: 1000, sort: { order: DESC, fields: [frontmatter___date] }) {
+      allMarkdownRemark(
+        limit: 1000
+        sort: { order: DESC, fields: [frontmatter___title] }
+      ) {
         edges {
           node {
             excerpt(pruneLength: 400)
@@ -18,45 +21,85 @@ exports.createPages = ({actions, graphql}) => {
             }
             frontmatter {
               title
-              cover
-              tags
               templateKey
-              date(formatString: "MMMM DD, YYYY")
             }
           }
         }
       }
     }
-  `).then(result => {
+  `).then((result) => {
     if (result.errors) {
-      result.errors.forEach(e => console.error(e.toString()))
-      return Promise.reject(result.errors)
+      result.errors.forEach((e) => console.error(e.toString()));
+      return Promise.reject(result.errors);
     }
 
-    const postsAndPages = result.data.allMarkdownRemark.edges
+    // const postsAndPages = result.data.allMarkdownRemark.edges
 
+    // // Post pages:
+    // let posts = []
+    // // Iterate through each post/page, putting all found posts (where templateKey = article-page) into `posts`
+    // postsAndPages.forEach(edge => {
+    //   if (_.isMatch(edge.node.frontmatter, {'templateKey': 'article-page'})) {
+    //     posts = posts.concat(edge)
+    //   }
+    // })
+
+    // createPaginatedPages({
+    //   edges: posts,
+    //   createPage: createPage,
+    //   pageTemplate: 'src/templates/blog.js',
+    //   pageLength: 6, // This is optional and defaults to 10 if not used
+    //   pathPrefix: 'blog', // This is optional and defaults to an empty string if not used
+    //   context: {}, // This is optional and defaults to an empty object if not used
+    // })
+    // postsAndPages.forEach(edge => {
+    //   const id = edge.node.id
+    //   createPage({
+    //     path: edge.node.fields.slug,
+    //     tags: edge.node.frontmatter.tags,
+    //     component: path.resolve(
+    //       `src/templates/${String(edge.node.frontmatter.templateKey)}.js`
+    //     ),
+    //     // additional data can be passed via context
+    //     context: {
+    //       id,
+    //     },
+    //   })
+    // })
+
+    // Tag pages:
+    //     let tags = []
+    //     // Iterate through each post, putting all found tags into `tags`
+    //     postsAndPages.forEach(edge => {
+    //       if (_.get(edge, `node.frontmatter.tags`)) {
+    //         tags = tags.concat(edge.node.frontmatter.tags)
+    //       }
+    //     })
+    //     // Eliminate duplicate tags
+    //     tags = _.uniq(tags)
+
+    //     // Make tag pages
+    //     tags.forEach(tag => {
+    //       const tagPath = `/tags/${_.kebabCase(tag)}/`
+
+    //       createPage({
+    //         path: tagPath,
+    //         component: path.resolve(`src/templates/tags.js`),
+    //         context: {
+    //           tag,
+    //         },
+    //       })
+    //     })
+    //   })
+    // }
+
+    const post = result.data.allMarkdownRemark.edges;
     // Post pages:
-    let posts = []
-    // Iterate through each post/page, putting all found posts (where templateKey = article-page) into `posts`
-    postsAndPages.forEach(edge => {
-      if (_.isMatch(edge.node.frontmatter, {'templateKey': 'article-page'})) {
-        posts = posts.concat(edge)
-      }
-    })
 
-    createPaginatedPages({
-      edges: posts,
-      createPage: createPage,
-      pageTemplate: 'src/templates/blog.js',
-      pageLength: 6, // This is optional and defaults to 10 if not used
-      pathPrefix: 'blog', // This is optional and defaults to an empty string if not used
-      context: {}, // This is optional and defaults to an empty object if not used
-    })
-    postsAndPages.forEach(edge => {
-      const id = edge.node.id
+    post.forEach((edge) => {
+      const id = edge.node.id;
       createPage({
         path: edge.node.fields.slug,
-        tags: edge.node.frontmatter.tags,
         component: path.resolve(
           `src/templates/${String(edge.node.frontmatter.templateKey)}.js`
         ),
@@ -64,44 +107,20 @@ exports.createPages = ({actions, graphql}) => {
         context: {
           id,
         },
-      })
-    })
+      });
+    });
+  });
+};
 
-    // Tag pages:
-    let tags = []
-    // Iterate through each post, putting all found tags into `tags`
-    postsAndPages.forEach(edge => {
-      if (_.get(edge, `node.frontmatter.tags`)) {
-        tags = tags.concat(edge.node.frontmatter.tags)
-      }
-    })
-    // Eliminate duplicate tags
-    tags = _.uniq(tags)
-
-    // Make tag pages
-    tags.forEach(tag => {
-      const tagPath = `/tags/${_.kebabCase(tag)}/`
-
-      createPage({
-        path: tagPath,
-        component: path.resolve(`src/templates/tags.js`),
-        context: {
-          tag,
-        },
-      })
-    })
-  })
-}
-
-exports.onCreateNode = ({node, actions, getNode}) => {
-  const {createNodeField} = actions
+exports.onCreateNode = ({ node, actions, getNode }) => {
+  const { createNodeField } = actions;
 
   if (node.internal.type === `MarkdownRemark`) {
-    const value = createFilePath({node, getNode})
+    const value = createFilePath({ node, getNode });
     createNodeField({
       name: `slug`,
       node,
       value,
-    })
+    });
   }
-}
+};
